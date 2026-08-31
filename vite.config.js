68408da567e2f44a3e022c34cf033d7fd86ef4c9
runny-life(Nikita) <runny_life@icloud.com>
2026-08-31 19:33:41 +0300

    feat: add prettier, tailwindcss and configurations

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+    server: {
+        open: true,
+    },
+    plugins: [
+        tailwindcss(),
+    ],
+})
